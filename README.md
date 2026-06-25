ad732c0f2a98ca1b94538cce1d78e25d603bc891
Pvilchisrizo <palomichys@gmail.com>
2026-06-25 12:17:12 -0600

    Innital Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Local chatbot using Ollama serve
+Run this in terminal : python3 -m http.server 8000
---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("chat-input-form");
+const responseBox = document.getElementById("chatbox-response");
+const promptBox = document.getElementById("prompt");
+
+form.onsubmit = async (event) => {
+  event.preventDefault();
+
+  const prompt = promptBox.value;
+  promptBox.value = "";
+  responseBox.innerText = "...";
+
+  const response = await fetch(`http://localhost:11434/api/generate`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      model: "llama3.2",
+      prompt: `${prompt}`,
+      stream: false,
+    }),
+  });
+
+  const data = await response.json();
+  responseBox.innerText = data.response;
+};
